7894cc5200e93a308e19e9810fb20ceb97ca8153
vincent <vincent@vincent-Precision-WorkStation-390>
2013-06-07 13:56:18 +0200

    ajout de l'affichage des erreur d'analyse

--- lib/toolkit/toolkit.php
+++ lib/toolkit/toolkit.php
@@ -601,7 +601,10 @@ set_error_handler(function($errno, $errstr, $errfile = '', $errline = 0, array $
 //gestion des erreurs fatales
 register_shutdown_function(function(){
     if(($err_array=error_get_last())){
-        if($err_array['type'] === E_ERROR || $err_array['type'] === E_USER_ERROR){
+        if($err_array['type'] === E_ERROR 
+                || $err_array['type'] === E_USER_ERROR
+                || $err_array['type'] === E_CORE_ERROR
+                || $err_array['type'] === E_PARSE){
             Output::instance()->clean();
             extract($err_array);
             require __DIR__.DS.'templates'.DS.'error'.DS.'fatal.php';
