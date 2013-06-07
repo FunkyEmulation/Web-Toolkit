34f9ee6523794afe98a8030bc80203efcedd8d90
vincent <vincent@vincent-Precision-WorkStation-390>
2013-06-07 11:55:29 +0200

    error : ajout d'un margin à la vue

--- lib/toolkit/templates/error/error.php
+++ lib/toolkit/templates/error/error.php
@@ -1,4 +1,4 @@
-<div style="border: 1px solid #A00;padding: 5px;">
+<div style="border: 1px solid #A00;padding: 5px;margin: 3px;">
     <h3>Une (petite) erreur est survenue !</h3>
     <b><?php echo $type?></b> : <?php echo $message?>
     <li><u>Fichier :</u> <em><?php echo $file?></em></li>
